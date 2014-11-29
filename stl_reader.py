44a93118f94f7429b75b3c5d8055b0960aab8027
christian <rcvpedersen@gmail.com>
2014-11-29 11:40:27 -0800

    Add feature to read ASCII STL files.

--- stl_reader.py
+++ stl_reader.py
@@ -41,10 +41,49 @@ def BinarySTL(fname):
 
     return Header, Points, Normals, Vertex1, Vertex2, Vertex3
 
+def AsciiSTL(fname):
+    with open(fname, 'r') as input_data:
+        # Skips text before the beginning of the interesting block:
+        init = False
+        triangles = []
+        verticies = []
+        for line in input_data:
+            print("in line")
+            if line.strip() == 'outer loop':  # Or whatever test is needed
+                init = True
+                verticies = []
+                continue
+            # Reads text until the end of the block:
+            elif line.strip() == 'endloop':
+                init = False
+                triangles.append(verticies)
+                continue
+            elif init:
+                words = line.strip().split(' ')
+                print(words)
+                assert words[0] == 'vertex'
+                verticies.append((float(words[1]), float(words[2]), float(words[3])))
+
+    return triangles
+
+def IsAsciiStl(fname):
+    with open(fname,'rb') as input_data:
+        line = input_data.readline()
+        if line[:3] == b'VCG' or line[:5] == b'COLOR':
+            #binary
+            return False
+        elif line[:5] == b'solid':
+            return True
+        else:
+            raise RuntimeError("Couldn't decide whet type of STL file this is ", line[:10])
 
 def read_stl_verticies(fname):
-    head, p, n, v1, v2, v3 = BinarySTL(fname)
-    for i, j, k in zip(v1, v2, v3):
-        yield (i, j, k)
+    if IsAsciiStl(fname):
+        for (i,j,k) in AsciiSTL(fname):
+            yield (i,j,k)
+    else:
+        head, p, n, v1, v2, v3 = BinarySTL(fname)
+        for i, j, k in zip(v1, v2, v3):
+            yield (i, j, k)
 
 
