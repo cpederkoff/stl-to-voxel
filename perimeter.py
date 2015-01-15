26774b153f8ede1df0646da5d650e8a6f97087f1
christian <rcvpedersen@gmail.com>
2015-01-14 22:21:39 -0800

    Optimizing the fill operation.

--- perimeter.py
+++ perimeter.py
@@ -8,28 +8,29 @@ def fillPerimeter(lineList, pixels):
     for x in range(len(pixels)):
         isBlack = False
         lines = list(findRelevantLines(lineList, x))
-
+        targetYs = list(map(lambda line:int(generateY(line,x)),lines))
         for y in range(len(pixels[x])):
             if isBlack:
                 pixels[x][y] = True
-            for line in lines:
-                if onLine(line, x, y):
-                    isBlack = not isBlack
-                    pixels[x][y] = True
+            if y in targetYs:
+                for line in lines:
+                    if onLine(line, x, y):
+                        isBlack = not isBlack
+                        pixels[x][y] = True
 
         if isBlack:
-            print("an error has occured at x%sz%s"%(x,line[2]))
+            print("an error has occured at x%sz%s"%(x,lineList[0][0][2]))
 
 
-def findRelevantLines(lineList, x):
+def findRelevantLines(lineList, x, ind=0):
     for line in lineList:
         same = False
         above = False
         below = False
         for pt in line:
-            if pt[0] > x:
+            if pt[ind] > x:
                 above = True
-            elif pt[0] == x:
+            elif pt[ind] == x:
                 same = True
             else:
                 below = True
@@ -38,17 +39,30 @@ def findRelevantLines(lineList, x):
         elif same and above:
             yield line
 
-def onLine(line, x, y):
+
+def generateY(line, x):
+    if line[1][0] == line[0][0]:
+        return -1
     ratio = (x - line[0][0]) / (line[1][0] - line[0][0])
     ydist = line[1][1] - line[0][1]
-    newy = line[0][1] + ratio*ydist
-    xgood = False
-    ygood = False
-    if int(line[0][0]) == x or int(line[1][0]) == x or max(line[0][0], line[1][0]) >= x and min(line[0][0], line[1][0]) <= x:
-        xgood = True
-    if int(line[0][1]) == y or int(line[1][1]) == y or max(line[0][1], line[1][1]) >= y and min(line[0][1], line[1][1]) <= y:
-        ygood = True
-    if int(newy) == y and xgood and ygood:
-        return True
-    else:
+    newy = line[0][1] + ratio * ydist
+    return newy
+
+
+def onLine(line, x, y):
+    newy = generateY(line, x)
+    if int(newy) != y:
         return False
+    if int(line[0][0]) != x and int(line[1][0]) != x and (max(line[0][0], line[1][0]) < x or min(line[0][0], line[1][0]) > x):
+        return False
+    if int(line[0][1]) != y and int(line[1][1]) != y and (max(line[0][1], line[1][1]) < y or min(line[0][1], line[1][1]) > y):
+        return False
+    return True
+
+def intersections(lines, x):
+    ret = []
+    for line in lines:
+        ratio = (x - line[0][0]) / (line[1][0] - line[0][0])
+        ydist = line[1][1] - line[0][1]
+        newy = line[0][1] + ratio*ydist
+    # find all intersection points, then skip all the calls to onLine (skip all white pixels)
