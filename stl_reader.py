c7df58f54d73ab71fca7f81b117e8e0185fbaf26
Marvin.Noll <Marvin.Noll@bruker.com>
2018-06-19 15:15:17 +0200

    added comment to previous edit

--- stl_reader.py
+++ stl_reader.py
@@ -59,8 +59,12 @@ def AsciiSTL(fname):
             elif init:
                 words = line.strip().split(' ')
                 assert words[0] == 'vertex'
-                verticies.append((float(words[3]), float(words[5]), float(words[7])))
-                #verticies.append((float(words[1]), float(words[2]), float(words[3])))
+                if(words[1]):
+                     verticies.append((float(words[1]), float(words[2]), float(words[3])))
+                else:
+                     #for STL files in which 'words' looks like(from Solidworks):
+                     #['vertex', '', '', '1', '', '2', '', '3']
+                     verticies.append((float(words[3]), float(words[5]), float(words[7])))
 
     return triangles
 
