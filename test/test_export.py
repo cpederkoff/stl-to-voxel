a72e7344d90d508d32efb00af3aaefc85ba8f9b4
Christian <cpederkoff@MBP-CPED.local>
2021-05-11 20:44:31 -0700

    save

--- test/test_export.py
+++ test/test_export.py
@@ -1,5 +1,4 @@
 import tempfile
-import filecmp
 import os
 import unittest
 
@@ -27,23 +26,8 @@ class TestStlToVoxel(unittest.TestCase):
             # Provided by @silverscorpio in PR #18
             doExport('data/test2.stl', os.path.join(tmpDir, 'test2.png'), 100)
 
-    def test_resolution(self):
-        with tempfile.TemporaryDirectory() as tmpDir:
-            for i in range(1, 100):
-                print('resolution:', i)
-                doExport('data/Pyramid.stl', os.path.join(tmpDir, 'Pyramid.png'), i)
-
-    def test_doExport(self):
-        with tempfile.TemporaryDirectory() as tmpDir:
-            outputFile = os.path.join(tmpDir, 'stanford_bunny.png')
-            doExport('data/Stanford_Bunny.stl', outputFile, 100)
-            expectedFiles = sorted(os.listdir('data/stanford_bunny_golden_slices'))
-            actualFiles = sorted(os.listdir(tmpDir))
-
     def test_resolution():
         for i in range(1, 100):
             print('resolution:', i)
             with tempfile.TemporaryDirectory() as tmpDir:
                 doExport('data/Pyramid.stl', os.path.join(tmpDir, 'Pyramid.png'), i)
-                doExport('data/Menger_sponge_sample.stl', os.path.join(tmpDir, 'Menger_sponge_sample.png'), i)
-                doExport('data/Eiffel_tower_sample.STL', os.path.join(tmpDir, 'Eiffel_tower_sample.png'), i)
