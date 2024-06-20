4523c031ab3ee55702ed97f251fa67a9fa3f70ea
Christian Pederkoff <6548797+cpederkoff@users.noreply.github.com>
2024-06-20 13:28:24 -0700

    works

--- stltovoxel/winding_query.py
+++ stltovoxel/winding_query.py
@@ -81,61 +81,7 @@ def negatan(f1):
     y,x = f1
     return -y, x
 
-# calculate baseline from angle of last two line segments of all incomplete loops
-## Find a more pleasing way to represent baseline. Angles are good, but the asymmetry is uncomfortable.
-# Look at neighbors distance and angle, incorporate baseline, decide who to connect to.
-# 
-def is_right(line, point):
-    a, b = line
-    x1, y1 = a
-    x2, y2 = b
-    x0, y0 = point
-    num = ((x2 - x1)*(y1 - y0) - (x1 - x0)*(y2 - y1))
-    return num > 0
-
-def corners(segs, pt, step_towards):
-    x, y = pt
-    # Without a small movement toward the other point the value is undefined
-    # because we are at the center of a sweep
-    dx, dy = step_towards
-    x += dx/1000
-    y += dy/1000
-    ret = 0
-    for i in range(len(segs) - 1):
-        s1 = segs[i]
-        s2 = segs[i+1]
-        assert s1[1] == s2[0]
-        a = np.array(list(s1[0]))
-        b = np.array(list(s1[1]))
-        c = np.array(list(s2[1]))
-        l1 = a - b
-        l2 = b - c
-        
-        ang = math.atan2(*atandiff((l2[1], l2[0]), (l1[1], l1[0])))
-        
-        if ang < 0 : 
-            # Concave case 
-            if not is_right((a, b), (x,y)) and not is_right((c, b), (x,y)):
-                ret += ang + 2 * math.pi
-            else: 
-                ret += ang 
-        else :
-            # Convex case (normal)
-            if is_right((a, b), (x,y)) and is_right((c, b), (x,y)):
-                ret += ang - 2 * math.pi
-            else: 
-                ret += ang 
-    return ret
-
-
 def edge_start(me_seg, them_pt):
-    # Starter unpaired point
-    s1, s2 = me_seg
-    angle = (s1[1] - s2[1], s1[0] - s2[0])
-    p1 = (s1[1] - them_pt[1], s1[0] - them_pt[0])
-    return atansum(negatan(p1), angle)
-
-def edge_start_no_atan(me_seg, them_pt):
     # Starter unpaired point
     s1, s2 = me_seg
     angle = (s1[1] - s2[1], s1[0] - s2[0])
@@ -159,63 +105,13 @@ def edge_end_baseline(me_seg):
     angle = (s1[1] - s2[1], s1[0] - s2[0])
     return math.atan2(*angle)
 
-def line(segs, inc):
-    im = np.zeros((100,100))
-    for x in range(-50, 50):
-        for y in range(-50, 50):
-            answers = []
-            for p1, p2 in segs:
-                x1, y1 = p1
-                x2, y2 = p2
-                angle = (y2 - y1, x2 - x1)
-                p1 = (y-y1, x-x1)
-                p2 = (y-y2, x-x2)
-                answers.append(atansum(negatan(p1), angle))
-                answers.append(atansum(p2, negatan(angle)))
-            accum = 0
-            for i in inc:
-                accum += math.atan2(*answers[i])
-
-            im[y+50][x+50] = accum
-    return im
-
-def get_winding(pos, segs):
-    accum = 0
-    for seg in segs:
-        accum += edge_start(seg, pos)
-        accum += edge_end(seg, pos)
-    return accum
-
-def get_direction(pos, segs, dangling_end):
-    accum = 0
-    for seg in segs:
-        accum += edge_start(seg, pos)
-        accum += edge_end(seg, pos)
-    # accum += edge_start(dangling_end, pos)
-
-    base = edge_end_baseline(dangling_end)
-    return base - accum
-
 def get_direction_backwards(pos, segs, dangling_start):
     accum = edge_start_baseline(dangling_start)
     for seg in segs:
         accum = atansum(accum, edge_start(seg, pos))
         accum = atansum(accum, edge_end(seg, pos))
-
-    return math.atan2(*accum)
-
-def get_direction_iter(pos, segs, dangling_end, target):
-    accum = 0
-    for seg in segs:
-        accum += edge_start(seg, pos)
-        accum += edge_end(seg, pos)
-    
-    # accum += edge_start(dangling_end, pos)
-
-    base = edge_end_baseline(dangling_end)
-    # target - accum is a compensation factor to point back toward the ideal winding number
-    print(accum)
-    return (base - accum) #+ (target - accum)
+        accum = vecnorm(accum)
+    return accum
 
 def find_polyline_endpoints(segs):
     start_to_end = dict()
@@ -276,9 +172,7 @@ def dist(p1, p2):
     return math.sqrt((y2 - y1)**2 + (x2 - x1)**2)
 
 def find_initial_angle(start, other_segs, my_seg):
-    angle_forward = get_direction_backwards(start, other_segs, my_seg)
-    delta_x = math.cos(angle_forward)
-    delta_y = math.sin(angle_forward)
+    delta_y, delta_x = get_direction_backwards(start, other_segs, my_seg)
     delta = np.array([delta_x, delta_y])
     return delta
 
