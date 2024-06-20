f418e7c4274c5021ede806f124e98b32ae3e9dcf
Christian Pederkoff <6548797+cpederkoff@users.noreply.github.com>
2024-06-19 20:38:36 -0700

    simplify

--- stltovoxel/playground.py
+++ stltovoxel/playground.py
@@ -2,9 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-def normalize(num):
-    return ((num + math.pi) % (2*math.pi)) - math.pi
-
 def atansum(f1, f2):
     y, x = f1
     z, w = f2
@@ -19,32 +16,30 @@ def negatan(f1):
     y,x = f1
     return -y, x
 
+def edge_start_baseline(s1, s2):
+    return (s1[1] - s2[1], s1[0] - s2[0])
+
 def edge_start(me_seg, them_pt):
     # Starter unpaired point
     s1, s2 = me_seg
-    angle = (s1[1] - s2[1], s1[0] - s2[0])
+    angle = edge_start_baseline(s1, s2)
     p1 = (s1[1] - them_pt[1], s1[0] - them_pt[0])
-    return math.atan2(*atansum(negatan(p1), angle))
+    return atansum(negatan(p1), angle)
 
 def edge_end(me_seg, them_pt):
     # Starter unpaired point
     s1, s2 = me_seg
-    angle = (s1[1] - s2[1], s1[0] - s2[0])
+    angle = edge_start_baseline(s1, s2)
     p1 = (s2[1] - them_pt[1], s2[0] - them_pt[0])
-    return math.atan2(*atansum(p1, negatan(angle)))
-
-def edge_start_baseline(me_seg):
-    s1, s2 = me_seg
-    angle = (s2[1] - s1[1], s2[0] - s1[0])
-    return math.atan2(*angle)
+    return atansum(p1, negatan(angle))
 
 
-def get_direction_backwards(pos, segs, dangling_start):
-    accum = edge_start_baseline(dangling_start)
+def get_direction(pos, segs, dangling_start):
+    accum = edge_start_baseline(dangling_start[1], dangling_start[0])
     for seg in segs:
-        accum += edge_start(seg, pos)
-        accum += edge_end(seg, pos)
-    return accum
+        accum = atansum(accum, edge_start(seg, pos))
+        accum = atansum(accum, edge_end(seg, pos))
+    return math.atan2(*accum)
 
 
 def angle_to_delta(theta):
@@ -85,54 +80,42 @@ def find_polyline_endpoints(segs):
     return start_to_end
 
 
-def grad(ox,oy,pt):
-    px,py = pt
-    x = ox - px
-    y = oy - py
-    gx = -((y)/(x**2 + y**2))
-    gy = x/(x**2 + y**2)
-    return (gx, gy)
-
-def accum_grad_90(x, y, segs):
-    ax = 0
-    ay = 0
+def grad_zero(pos, monopole, repel=True):
+    dir = pos - monopole
+    denom = (dir[0]**2 + dir[1]**2)
+    grad = dir / denom
+    if repel:
+        return grad
+    else:
+        return -grad
+    
+def accum_grad_zero(pt, segs):
+    acc = np.array([0.0,0.0])
     for start, end in segs:
-        # for pt in [start, end]:
-        gx, gy = grad(x+0.00001,y+0.00001,start)
-        ax += gy
-        ay -= gx
-        gx, gy = grad(x+0.00001,y+0.00001,end)
-        ax -= gy
-        ay += gx
-    return (ax, ay)
+        acc += grad_zero(pt,start, repel=True)
+        acc += grad_zero(pt,end, repel=False)
+    return acc
 
 def vecnorm(pt):
-    x, y = pt
-    dist = math.sqrt(x**2 + y**2)
-    return (x / dist, y / dist)
-
-
+    dist = math.sqrt(pt[0]**2 + pt[1]**2)
+    return pt/dist
 
 start_to_end = find_polyline_endpoints(segs)
-# print(start_to_end)
 
 start = list(start_to_end.keys())[0]
-end = start_to_end[start]
-# find the segment I am a part of
 my_seg = next(filter(lambda seg: seg[0] == start, segs))
 # find all other segments
 other_segs = list(filter(lambda seg: seg[0] != start, segs))
-target = math.pi
-angle_forward = get_direction_backwards(start, other_segs, my_seg) + target + math.pi
+angle_forward = get_direction(start, other_segs, my_seg)
 delta = angle_to_delta(angle_forward)
-pos = start + (delta * 0.1)
+print(delta)
+pos = start + (delta * 0.0001)
+
 plt.quiver(*start, *delta, color=['r','b','g'], scale=21)
-for i in range(100):
-    delt = vecnorm(accum_grad_90(*pos, segs))
+for i in range(10):
+    delt = vecnorm(accum_grad_zero(pos, segs))
+    plt.quiver(*pos, *(delt), color=['r','b','g'], scale=21)
     plt.plot([pos[0], pos[0] + delt[0]], [pos[1], pos[1] + delt[1]], 'bo', linestyle="-")
     pos += delt
-
-
-# plt.imshow(background, aspect='auto', cmap='viridis', extent=[-20, 20, -20, 20], vmin=-math.pi, vmax=math.pi*2)
 
 plt.show()
